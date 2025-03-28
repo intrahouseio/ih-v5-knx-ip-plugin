dde18b4393aae986a3b46a15ce6d202a3603aef6
houseoffuture <31938510+houseoffuture@users.noreply.github.com>
2025-03-28 17:39:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
 const util = require('util');
 
 //const plugin = require('ih-plugin-api')();
-const app = require('./app'); 
+const app = require('./app');
 
 (async () => {
-  
-  let plugin;
 
-  try {
-    const opt = getOptFromArgs();
-    const pluginapi = opt && opt.pluginapi ? opt.pluginapi : 'ih-plugin-api';
-    plugin = require(pluginapi + '/index.js')();
-    plugin.log('Plugin is start.', 1);
+    let plugin;
 
-    plugin.params.data = await plugin.params.get();
-    plugin.log('Recieve plugin parametrs: '+ util.inspect(plugin.params.data), 1);
+    try {
+        const opt = getOptFromArgs();
+        const minversion = '5.17.82';
+        const version = opt.version;
+        const pluginapi = opt && opt.pluginapi ? opt.pluginapi : 'ih-plugin-api';
+        plugin = require(pluginapi + '/index.js')();
 
-    plugin.channels.data = await plugin.channels.get();
-    plugin.log('Recieve plugin channels: '+ util.inspect(plugin.channels.data), 1);
+        if (version >= minversion) {
+            plugin.log('Plugin is start.');
 
-    app(plugin);
+            plugin.params = await plugin.params.get();
+            plugin.log('Recieve plugin parametrs: ' + util.inspect(plugin.params), 1);
 
-  } catch (err) {
-    plugin.exit(8, `Error: ${util.inspect(err)}`);
-  }
+            plugin.channels = await plugin.channels.get();
+            plugin.log('Recieve plugin channels: ' + util.inspect(plugin.channels), 1);
+
+            plugin.logger.setParams({ logsize: plugin.params.logsize, logrotate: plugin.params.logrotate });
+
+            app(plugin);
+        }
+        else {
+            plugin.log("Plugin not start! Update system to version: " + minversion + " || Current version: " + version);
+            plugin.exit(17, `Error: system version!`);
+        }
+
+    } catch (err) {
+        plugin.exit(8, `Error: ${util.inspect(err)}`);
+    }
 })();
 
 function getOptFromArgs() {
-  let opt;
-  try {
-    opt = JSON.parse(process.argv[2]); //
-  } catch (e) {
-    opt = {};
-  }
-  return opt;
+    let opt;
+    try {
+        opt = JSON.parse(process.argv[2]); //
+    } catch (e) {
+        opt = {};
+    }
+    return opt;
 }
